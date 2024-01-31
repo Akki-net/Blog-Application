5f18febaf253df03ff33155b2592cb6f92a38b30
Akki-net <akashsharma.zak@gmail.com>
2024-01-31 12:07:05 +0530

    modified the URL patterns and views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Post
-from django.http import Http404
 
 def post_list(request):
     posts = Post.published.all()
@@ -8,16 +7,13 @@ def post_list(request):
                 'blog/post/list.xhtml',
                 {'posts': posts})
 
-def post_detail(request, id):
-    # try:
-    #     post = Post.published.get(id=id)
-    # except Post.DoesNotExist:
-    #     raise Http404('No Post found.')
-
-    # alternative way
+def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post,  
-                        id=id, 
-                        status=Post.Status.PUBLISHED)
+                        status=Post.Status.PUBLISHED,
+                        slug=post,
+                        publish__year=year,
+                        publish__month=month,
+                        publish__day=day)
     return render(request,
                 'blog/post/detail.xhtml',
                 {'post': post})
